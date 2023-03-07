9d4a34657b23398004f5c33afac8aebb9997aa8b
sujairamkpr <sujairamkpr@gmail.com>
2023-03-07 19:58:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react"
+
+import "./styles.css"
+
 
 function App() {
+  const[count,setCount]=useState(0)
+  const[num1,setNum1]=useState()
+const[num2,setNum2]=useState()
+
+  const handleAdd=()=>{
+setCount(num1+num2)
+
+  }
+  const handleSub=()=>{
+    
+   
+setCount(num1-num2)
+
+  }
+  const handleMul=()=>{
+  
+   setCount(num1*num2)
+  }
+  const handleDiv=()=>{
+  setCount(num1/num2)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div  className="cc-grid">
+      <div className='result'>
+      <div>{count}</div>
+      </div>
+<button  className="span2">Ac</button>
+<button className="span2">Del</button>
+<button onClick={()=>{
+  handleDiv()
+}}>/</button>
+
+<button 
+>7</button>
+<button >8</button>
+<button >9</button>
+
+<button onClick={()=>{
+  handleMul()
+}}>*</button>
+<button >4</button>
+<button >5</button>
+<button>6</button>
+<button onClick={()=>{
+handleSub()
+  
+}} >-</button>
+<button>1</button>
+<button>2</button>
+<button>3</button>
+<button onClick={()=>{
+  handleAdd()
+}} >+</button>
+<button>0</button>
+
+<button>.</button>
+<button className="span2">=</button>
+
+</div>
   );
 }
 
